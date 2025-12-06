5a70c45dd6782b0c935175d8ee59380787518777
4butler <158169581+4butler@users.noreply.github.com>
2025-12-07 07:00:27 +0900

    Add files via upload

--- 255/js/map.js
+++ 255/js/map.js
@@ -28,34 +28,128 @@ let backcontinents = [
   document.getElementById('backantarctica')
 ];
 
-let closeBtns = document.querySelectorAll('.close');
+let closeBtns = document.getElementById('close');
 let check = -1;
 
 backcontinents.forEach((con, num) => {
   con.addEventListener('click', function () {
     check = num;
-    continents[num].classList.toggle('Active');
-    panels[num].classList.toggle('open');
 
-    continents.forEach((c, i) => {
-      if (i === check) return;
-      c.classList.toggle('hide');
-    });
+    if (num === 0) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('asiaActive');
+        backcontinents[i].classList.add('asiaActive');
+      }
+      panels[num].classList.add('open');
+    }
+
+    if (num === 1) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('africaActive');
+        backcontinents[i].classList.add('africaActive');
+      }
+      panels[num].classList.add('open');
+    }
+
+    if (num === 2) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('europeActive');
+        backcontinents[i].classList.add('europeActive');
+      }
+      panels[num].classList.add('open');
+    }
+
+    if (num === 3) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('northamericaActive');
+        backcontinents[i].classList.add('northamericaActive');
+      }
+      panels[num].classList.add('open');
+    }
+
+    if (num === 4) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('southamericaActive');
+        backcontinents[i].classList.add('southamericaActive');
+      }
+      panels[num].classList.add('open');
+    }
+
+    if (num === 5) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('oceaniaActive');
+        backcontinents[i].classList.add('oceaniaActive');
+      }
+      panels[num].classList.add('open');
+    }
+
+    if (num === 6) {
+      for (let i = 0; i < 7; i++) {
+        continents[i].classList.add('antarcticaActive');
+        backcontinents[i].classList.add('antarcticaActive');
+      }
+      panels[num].classList.add('open');
+    }
+
   });
 });
 
-closeBtns.forEach(button => {
-  button.addEventListener('click', function () {
-    if (check === -1) return;
-
-    continents[check].classList.remove('Active');
+closeBtns.addEventListener('click', function () {
+  if (check === 0) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('asiaActive');
+      backcontinents[i].classList.remove('asiaActive');
+    }
     panels[check].classList.remove('open');
+  }
 
-    continents.forEach((c, i) => {
-      if (i === check) return;
-      c.classList.remove('hide');
-    });
+  if (check === 1) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('africaActive');
+      backcontinents[i].classList.remove('africaActive');
+    }
+    panels[check].classList.remove('open');
+  }
 
-    check = -1;
-  });
+  if (check === 2) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('europeActive');
+      backcontinents[i].classList.remove('europeActive');
+    }
+    panels[check].classList.remove('open');
+  }
+
+  if (check === 3) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('northamericaActive');
+      backcontinents[i].classList.remove('northamericaActive');
+    }
+    panels[check].classList.remove('open');
+  }
+
+  if (check === 4) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('southamericaActive');
+      backcontinents[i].classList.remove('southamericaActive');
+    }
+    panels[check].classList.remove('open');
+  }
+
+  if (check === 5) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('oceaniaActive');
+      backcontinents[i].classList.remove('oceaniaActive');
+    }
+    panels[check].classList.remove('open');
+  }
+
+  if (check === 6) {
+    for (let i = 0; i < 7; i++) {
+      continents[i].classList.remove('antarcticaActive');
+      backcontinents[i].classList.remove('antarcticaActive');
+    }
+    panels[check].classList.remove('open');
+  }
+
+  check = -1;
 });
--- 255/js/planets.js
+++ 255/js/planets.js
@@ -39,11 +39,29 @@ let satellite = [
     document.getElementById('uranus-titania'),
 ]
 
+let planetContent = document.querySelectorAll(".planetContent");
+let satelliteContent = document.querySelectorAll(".satelliteContent");
 let onfbtn = document.getElementById('switch');
 let sun = document.getElementById('sun');
 let earthDetailyBtn = document.getElementById('earth_detaily');
 let closeBtns = document.getElementById('close');
 let check = -1;
+let panelCheck = 0;
+let scrollBackup = { x: 0, y: 0 };
+
+function saveScroll() {
+    scrollBackup.x = window.scrollX;
+    scrollBackup.y = window.scrollY;
+}
+
+function restoreScroll() {
+    window.scrollTo({
+        top: scrollBackup.y,
+        left: scrollBackup.x,
+        behavior: "smooth"
+    });
+}
+
 
 onfbtn.addEventListener('change', () => {
     if (onfbtn.checked) {
@@ -61,10 +79,20 @@ onfbtn.addEventListener('change', () => {
 
 planet.forEach((plan, num) => {
     plan.addEventListener('click', function () {
+        if (panelCheck) {
+            panelCheck = 1;
+            satelliteContent.forEach(i => i.classList.remove("Active"));
+            planetContent.forEach(i => i.classList.add("Active"));
+        }
+
+        saveScroll();
         check = num;
+        panelCheck = 1;
+
         plan.classList.add('Active');
         panel[num].classList.add('open');
         closeBtns.classList.add('Active');
+        planetContent.forEach(i => i.classList.add("Active"));
 
         planet.forEach((p, i) => {
             if (i === check) return;
@@ -74,15 +102,31 @@ planet.forEach((plan, num) => {
         if (2 <= num <= 6) {
             satellite[num - 2].classList.add('Active');
         }
+        window.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+
     });
 });
 
-closeBtns.addEventListener('click', function () {
-    if (check === -1) return;
+satellite.forEach((sat, num) => {
+    sat.addEventListener('click', function () {
+        panelCheck = 2;
+        planetContent.forEach(i => i.classList.remove("Active"));
+        satelliteContent.forEach(i => i.classList.add("Active"));
+    })
+})
 
+closeBtns.addEventListener('click', function () {
+    restoreScroll();
+
+    panelCheck = 0;
     planet[check].classList.remove('Active');
     panel[check].classList.remove('open');
     closeBtns.classList.remove('Active');
+    planetContent.forEach(i => i.classList.remove("Active"));
+    satelliteContent.forEach(i => i.classList.remove("Active"));
 
     for (let i = 0; i < planet.length; i++) {
         if (i === check) continue;
